f549c2f0c561227d82e3656b65911109ceb452f5
mu-777 <ryosuke.murata.7@gmail.com>
2015-07-09 23:18:07 +0900

    add nodes overview images

--- rqt_sensor_viewers/setup.py
+++ rqt_sensor_viewers/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rqt_sensor_viewers'],
+    packages=['gui_nodes'],
     package_dir={'': 'src'}
 )
 setup(**d)
